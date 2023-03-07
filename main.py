2d5dbe88197d181ed1e3c625244c839a9f37e876
Fantiko <86378890+Fantiko@users.noreply.github.com>
2023-03-07 14:03:03 -0300

    finalizando o projeto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import turtle
+import winsound as sound
+
 
 #   config iniciais
 wn = turtle.Screen()
@@ -6,6 +8,11 @@ wn.title("pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+#   Placar
+score_a = 0
+score_b = 0
+
 
 #   barra A
 barra_a = turtle.Turtle()
@@ -31,33 +38,33 @@ bola.speed(0)
 bola.shape("square")
 bola.color("white")
 bola.penup()
-bola.dx = 0.3
-bola.dy = .3
+bola.dx = 0.2
+bola.dy = 0.2
 
 
 
 #   funçoes
 def barra_a_up():
     y = barra_a.ycor()
-    y += 20
+    y += 30
     barra_a.sety(y)
 
 
 def barra_a_down():
     y = barra_a.ycor()
-    y -= 20
+    y -= 30
     barra_a.sety(y)
 
 
 def barra_b_up():
     y = barra_b.ycor()
-    y += 20
+    y += 30
     barra_b.sety(y)
 
 
 def barra_b_down():
     y = barra_b.ycor()
-    y -= 20
+    y -= 30
     barra_b.sety(y)
 
 
@@ -68,6 +75,18 @@ wn.onkeypress(barra_a_up, "w")
 wn.onkeypress(barra_a_down, "s")
 wn.onkeypress(barra_b_up, "8")
 wn.onkeypress(barra_b_down, "5")
+
+#   pen
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B:0", align="center", font=("Courier", 24, "normal"))
+
+
 
 #   main game loop
 while True:
@@ -85,7 +104,36 @@ while True:
         bola.dy *= -1
 
     if bola.xcor() > 390:
-        bola.goto(0)
+        bola.goto(0, 0)
+        bola.dx = 0.2
+        bola.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        sound.PlaySound("bounce.wav", sound.SND_ASYNC)
+        bola.dx = 0.2
 
     if bola.xcor() < -390:
-        bola.goto(0)
+        bola.goto(0, 0)
+        bola.dx = 0.2
+        bola.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        sound.PlaySound("bounce.wav", sound.SND_ASYNC)
+
+
+
+    #   colisao
+    if 340 < bola.xcor() < 350 and (bola.ycor() < barra_b.ycor() + 40) and (bola.ycor() > barra_b.ycor() - 40):
+        bola.setx(340)
+        bola.dx *= -1
+        bola.dx *= 1.03
+        sound.PlaySound("bounce.wav", sound.SND_ASYNC)
+
+    if -340 > bola.xcor() > -350 and (bola.ycor() < barra_a.ycor() + 40) and (bola.ycor() > barra_a.ycor() - 40):
+        bola.setx(-340)
+        bola.dx *= -1
+        bola.dx *= 1.03
+        sound.PlaySound("bounce.wav", sound.SND_ASYNC)
+
